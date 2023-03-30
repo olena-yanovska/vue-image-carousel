2099efb3330a0c7652e16a08d7613ad31073c5ec
Olena Yanovska <alena.yn.adw@gmail.com>
2023-03-30 10:54:34 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <ImageCarousel />
+</template>
+
+<script>
+import ImageCarousel from './components/ImageCarousel.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ImageCarousel
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/ImageCarousel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    <h1>Image Carousel</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageCarousel',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
